2e544baaa0f305266974cd344c26523cacfa7ef4
Meridith <meridith.junk@gmail.com>
2022-04-17 19:01:21 -0500

    start of building string builder

--- Json-Encoder/Json-Encoder/CustomEncoder.cs
+++ Json-Encoder/Json-Encoder/CustomEncoder.cs
@@ -1,9 +1,22 @@
+using System.Reflection;
+using System.Text;
+
 namespace Json_Encoder;
 
 public class CustomEncoder
 {
     public string Create(object objectToBeEncoded)
     {
-        return "";
+        var properties = objectToBeEncoded.GetType().GetProperties();
+        var stringBuilder = new StringBuilder();
+        foreach (var property in properties)
+        {
+            var name = property.Name;
+            var temp = objectToBeEncoded.GetType().GetProperty(name);
+            var value = (string) temp.GetValue(objectToBeEncoded, null);
+            stringBuilder.Append($"\"{name}\":\"{value}\"");
+        }
+        
+        return "{" + stringBuilder + "}";
     }
 }
